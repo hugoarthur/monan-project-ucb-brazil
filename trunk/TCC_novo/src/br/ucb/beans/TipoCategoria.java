8e814e69c3ae7746cbb1a708b16c01ba12704bfe
gzfreitas@gmail.com <gzfreitas@gmail.com@b4a6f1fb-a3e5-f0e7-49a2-75df67cf75d6>
2012-03-27 18:07:50 +0000

    projeto versão 0.1 escopo e telas prontos, problema na telaPrimeira, ela não está abrindo com o programa.

---
+++ trunk/TCC_novo/src/br/ucb/beans/TipoCategoria.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package br.ucb.beans;
+
+/**
+ *
+ * @author GUICUNHA
+ */
+public class TipoCategoria {
+    private Integer id_tipo_categoria;
+    private String nom_tipo_categoria;
+}
--- trunk/TCC_novo/src/br/ucb/beans/Usuario.java
+++ trunk/TCC_novo/src/br/ucb/beans/Usuario.java
@@ -15,4 +15,74 @@ public class Usuario {
     private String senha;
     private String nome;
     private String universidade;
+
+    /**
+     * @return the idUsuario
+     */
+    public Integer getIdUsuario() {
+        return idUsuario;
+    }
+
+    /**
+     * @param idUsuario the idUsuario to set
+     */
+    public void setIdUsuario(Integer idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    /**
+     * @return the login
+     */
+    public String getLogin() {
+        return login;
+    }
+
+    /**
+     * @param login the login to set
+     */
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    /**
+     * @return the senha
+     */
+    public String getSenha() {
+        return senha;
+    }
+
+    /**
+     * @param senha the senha to set
+     */
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the universidade
+     */
+    public String getUniversidade() {
+        return universidade;
+    }
+
+    /**
+     * @param universidade the universidade to set
+     */
+    public void setUniversidade(String universidade) {
+        this.universidade = universidade;
+    }
 }

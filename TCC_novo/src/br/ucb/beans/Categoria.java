d8d96b0e8d8ff29ac321b0a9c5c641eac0585fe8
gzfreitas@gmail.com <gzfreitas@gmail.com@b4a6f1fb-a3e5-f0e7-49a2-75df67cf75d6>
2012-03-27 18:07:50 +0000

    projeto versão 0.1 escopo e telas prontos, problema na telaPrimeira, ela não está abrindo com o programa.

---
+++ TCC_novo/src/br/ucb/beans/Categoria.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package br.ucb.beans;
+
+/**
+ *
+ * @author GUICUNHA
+ */
+public class Categoria {
+    private Integer idCategoria;
+    private String txt_categoria;
+}

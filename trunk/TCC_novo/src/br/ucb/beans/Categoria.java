e239298423993ca4149ed1d8b46fea04e0955972
gzfreitas@gmail.com <gzfreitas@gmail.com@b4a6f1fb-a3e5-f0e7-49a2-75df67cf75d6>
2012-03-27 18:12:21 +0000

    projeto versão 0.1 escopo e telas prontos, problema na telaPrimeira, ela não está abrindo com o programa.

--- trunk/TCC_novo/src/br/ucb/beans/Categoria.java
+++ trunk/TCC_novo/src/br/ucb/beans/Categoria.java
@@ -12,4 +12,32 @@ package br.ucb.beans;
 public class Categoria {
     private Integer idCategoria;
     private String txt_categoria;
+
+    /**
+     * @return the idCategoria
+     */
+    public Integer getIdCategoria() {
+        return idCategoria;
+    }
+
+    /**
+     * @param idCategoria the idCategoria to set
+     */
+    public void setIdCategoria(Integer idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    /**
+     * @return the txt_categoria
+     */
+    public String getTxt_categoria() {
+        return txt_categoria;
+    }
+
+    /**
+     * @param txt_categoria the txt_categoria to set
+     */
+    public void setTxt_categoria(String txt_categoria) {
+        this.txt_categoria = txt_categoria;
+    }
 }

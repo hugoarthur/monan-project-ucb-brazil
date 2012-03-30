a2e87282681db3bedae47ef7a8286dd5430dc379
gzfreitas@gmail.com <gzfreitas@gmail.com@b4a6f1fb-a3e5-f0e7-49a2-75df67cf75d6>
2012-03-30 18:15:49 +0000

    criacao de novas classes DAO

--- trunk/TCC_novo/src/br/ucb/beans/Projeto.java
+++ trunk/TCC_novo/src/br/ucb/beans/Projeto.java
@@ -15,5 +15,47 @@ public class Projeto {
    private Integer idProjeto;
    private String nomeProjeto;
    private Date dataProjeto;
+
+    /**
+     * @return the idProjeto
+     */
+    public Integer getIdProjeto() {
+        return idProjeto;
+    }
+
+    /**
+     * @param idProjeto the idProjeto to set
+     */
+    public void setIdProjeto(Integer idProjeto) {
+        this.idProjeto = idProjeto;
+    }
+
+    /**
+     * @return the nomeProjeto
+     */
+    public String getNomeProjeto() {
+        return nomeProjeto;
+    }
+
+    /**
+     * @param nomeProjeto the nomeProjeto to set
+     */
+    public void setNomeProjeto(String nomeProjeto) {
+        this.nomeProjeto = nomeProjeto;
+    }
+
+    /**
+     * @return the dataProjeto
+     */
+    public Date getDataProjeto() {
+        return dataProjeto;
+    }
+
+    /**
+     * @param dataProjeto the dataProjeto to set
+     */
+    public void setDataProjeto(Date dataProjeto) {
+        this.dataProjeto = dataProjeto;
+    }
    
 }

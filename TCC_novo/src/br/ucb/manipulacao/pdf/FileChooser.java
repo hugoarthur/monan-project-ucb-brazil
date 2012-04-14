adc62434e71c57734b85a101fd2a6db636409da0
hugo220390@gmail.com <hugo220390@gmail.com@b4a6f1fb-a3e5-f0e7-49a2-75df67cf75d6>
2012-04-14 01:58:36 +0000

    Colocação de filtro no FileChooser para mostrar somente arquivos pdf na busca para extração.

--- TCC_novo/src/br/ucb/manipulacao/pdf/FileChooser.java
+++ TCC_novo/src/br/ucb/manipulacao/pdf/FileChooser.java
@@ -1,20 +1,21 @@
 package br.ucb.manipulacao.pdf;
 
-
 import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
 
 public class FileChooser {
-        public String caminho (){            
-            String caminhoArquivo = "";
-            JFileChooser arquivo = new JFileChooser();
-            int retorno = arquivo.showOpenDialog(null);
-            if(retorno == JFileChooser.APPROVE_OPTION){
-                caminhoArquivo = arquivo.getSelectedFile().getAbsolutePath();
-            }else{
-                JOptionPane.showMessageDialog(null, "Voce nao selecionou nenhum Arquivo PDF.");
-                caminhoArquivo = "";
-            }
-                return caminhoArquivo;     
+
+    public String caminho() {
+        String caminhoArquivo = "";
+        JFileChooser arquivo = new JFileChooser();
+         arquivo.setFileFilter(new ExtensionFileFilter("Arquivos PDF","pdf"));
+        int retorno = arquivo.showOpenDialog(null);
+        if (retorno == JFileChooser.APPROVE_OPTION) {
+            caminhoArquivo = arquivo.getSelectedFile().getAbsolutePath();
+        } else {
+            JOptionPane.showMessageDialog(null, "Voce nao selecionou nenhum Arquivo PDF.");
+            caminhoArquivo = "";
         }
+        return caminhoArquivo;
+    }
 }

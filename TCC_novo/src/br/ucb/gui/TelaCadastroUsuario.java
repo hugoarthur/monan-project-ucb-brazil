caf2dc3b3f88fd47af10373f099301ef82a6d14b
gzfreitas@gmail.com <gzfreitas@gmail.com@b4a6f1fb-a3e5-f0e7-49a2-75df67cf75d6>
2012-04-19 21:18:55 +0000

    problemas no jtree

--- TCC_novo/src/br/ucb/gui/TelaCadastroUsuario.java
+++ TCC_novo/src/br/ucb/gui/TelaCadastroUsuario.java
@@ -12,6 +12,11 @@
 package br.ucb.gui;
 
 import br.ucb.gui.cadastro.CadastrarUsuario;
+import java.awt.Container;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.ComboBoxModel;
+import javax.swing.DefaultComboBoxModel;
 import javax.swing.JComboBox;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
@@ -35,13 +40,13 @@ public class TelaCadastroUsuario extends javax.swing.JFrame {
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
+        bindingGroup = new org.jdesktop.beansbinding.BindingGroup();
 
         jPanel2 = new javax.swing.JPanel();
         jPanel1 = new javax.swing.JPanel();
         NomeLabel = new javax.swing.JLabel();
         NomeTextField = new javax.swing.JTextField();
         UniversidadeLabel = new javax.swing.JLabel();
-        UniversidadeComboBox = new javax.swing.JComboBox();
         LoginLabel = new javax.swing.JLabel();
         LoginTextField = new javax.swing.JTextField();
         SenhaLabel = new javax.swing.JLabel();
@@ -49,6 +54,7 @@ public class TelaCadastroUsuario extends javax.swing.JFrame {
         Cadastrar = new javax.swing.JButton();
         Cancelar = new javax.swing.JButton();
         jLabel5 = new javax.swing.JLabel();
+        universidadeComboBox = new javax.swing.JComboBox();
 
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
@@ -62,14 +68,17 @@ public class TelaCadastroUsuario extends javax.swing.JFrame {
         );
 
         setTitle("CADASTRO - Sistema de Avaliação Formativa");
+        addContainerListener(new java.awt.event.ContainerAdapter() {
+            public void componentAdded(java.awt.event.ContainerEvent evt) {
+                formComponentAdded(evt);
+            }
+        });
 
         jPanel1.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "Novo Usuário", javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.DEFAULT_POSITION, null, java.awt.Color.darkGray));
 
         NomeLabel.setText("Nome : ");
 
         UniversidadeLabel.setText("Universidade :");
-
-        UniversidadeComboBox.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
 
         LoginLabel.setText("Login :");
 
@@ -83,8 +92,29 @@ public class TelaCadastroUsuario extends javax.swing.JFrame {
         });
 
         Cancelar.setText("Cancelar");
+        Cancelar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                CancelarActionPerformed(evt);
+            }
+        });
 
         jLabel5.setIcon(new javax.swing.ImageIcon(getClass().getResource("/br/ucb/img/logo_ucb.png"))); // NOI18N
+
+        universidadeComboBox.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+
+        org.jdesktop.beansbinding.Binding binding = org.jdesktop.beansbinding.Bindings.createAutoBinding(org.jdesktop.beansbinding.AutoBinding.UpdateStrategy.READ_WRITE, universidadeComboBox, org.jdesktop.beansbinding.ELProperty.create("${instituicao}"), universidadeComboBox, org.jdesktop.beansbinding.BeanProperty.create("selectedItem"), "");
+        bindingGroup.addBinding(binding);
+
+        universidadeComboBox.addContainerListener(new java.awt.event.ContainerAdapter() {
+            public void componentAdded(java.awt.event.ContainerEvent evt) {
+                universidadeComboBoxComponentAdded(evt);
+            }
+        });
+        universidadeComboBox.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                universidadeComboBoxActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -98,15 +128,15 @@ public class TelaCadastroUsuario extends javax.swing.JFrame {
                             .addGroup(jPanel1Layout.createSequentialGroup()
                                 .addComponent(NomeLabel)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(NomeTextField, javax.swing.GroupLayout.DEFAULT_SIZE, 197, Short.MAX_VALUE))
+                                .addComponent(NomeTextField, javax.swing.GroupLayout.DEFAULT_SIZE, 203, Short.MAX_VALUE))
                             .addGroup(jPanel1Layout.createSequentialGroup()
                                 .addComponent(UniversidadeLabel)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(UniversidadeComboBox, 0, 165, Short.MAX_VALUE))
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addComponent(universidadeComboBox, 0, 165, Short.MAX_VALUE))
                             .addGroup(jPanel1Layout.createSequentialGroup()
                                 .addComponent(LoginLabel)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(LoginTextField, javax.swing.GroupLayout.DEFAULT_SIZE, 202, Short.MAX_VALUE))
+                                .addComponent(LoginTextField, javax.swing.GroupLayout.DEFAULT_SIZE, 208, Short.MAX_VALUE))
                             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                                 .addComponent(Cadastrar)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
@@ -114,7 +144,7 @@ public class TelaCadastroUsuario extends javax.swing.JFrame {
                             .addGroup(jPanel1Layout.createSequentialGroup()
                                 .addComponent(SenhaLabel)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                                .addComponent(SenhaPasswordField, javax.swing.GroupLayout.DEFAULT_SIZE, 191, Short.MAX_VALUE))))
+                                .addComponent(SenhaPasswordField, javax.swing.GroupLayout.DEFAULT_SIZE, 197, Short.MAX_VALUE))))
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGap(67, 67, 67)
                         .addComponent(jLabel5)))
@@ -129,8 +159,8 @@ public class TelaCadastroUsuario extends javax.swing.JFrame {
                     .addComponent(NomeTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(UniversidadeLabel)
-                    .addComponent(UniversidadeComboBox, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(universidadeComboBox, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(UniversidadeLabel))
                 .addGap(18, 18, 18)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(LoginLabel)
@@ -155,7 +185,7 @@ public class TelaCadastroUsuario extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(19, Short.MAX_VALUE))
+                .addContainerGap(13, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -165,12 +195,33 @@ public class TelaCadastroUsuario extends javax.swing.JFrame {
                 .addContainerGap(17, Short.MAX_VALUE))
         );
 
+        bindingGroup.bind();
+
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void CadastrarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_CadastrarActionPerformed
-        Cadastrar.addActionListener(new CadastrarUsuario(this));
+        getCadastrar().addActionListener(new CadastrarUsuario(this));
     }//GEN-LAST:event_CadastrarActionPerformed
+
+    private void CancelarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_CancelarActionPerformed
+        // TODO add your handling code here:
+        setVisible(false);
+    }//GEN-LAST:event_CancelarActionPerformed
+
+    private void formComponentAdded(java.awt.event.ContainerEvent evt) {//GEN-FIRST:event_formComponentAdded
+        // TODO add your handling code here:
+
+    }//GEN-LAST:event_formComponentAdded
+
+    private void universidadeComboBoxActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_universidadeComboBoxActionPerformed
+        // TODO add your handling code here:
+        setItemUniverdidade();
+    }//GEN-LAST:event_universidadeComboBoxActionPerformed
+
+    private void universidadeComboBoxComponentAdded(java.awt.event.ContainerEvent evt) {//GEN-FIRST:event_universidadeComboBoxComponentAdded
+        // TODO add your handling code here:
+    }//GEN-LAST:event_universidadeComboBoxComponentAdded
 
     /**
     * @param args the command line arguments
@@ -179,6 +230,12 @@ public class TelaCadastroUsuario extends javax.swing.JFrame {
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new TelaCadastroUsuario().setVisible(true);
+                String [] instituicao = {"Universidade Federal Fluminense" ,
+                "Universidade Católica de Brasília", "Universidade Federal de Santa Catarina"};
+//                universidadeCombo = new JComboBox(instituicao);
+         
+                
+
             }
         });
     }
@@ -192,11 +249,12 @@ public class TelaCadastroUsuario extends javax.swing.JFrame {
     private javax.swing.JTextField NomeTextField;
     private javax.swing.JLabel SenhaLabel;
     private javax.swing.JPasswordField SenhaPasswordField;
-    private javax.swing.JComboBox UniversidadeComboBox;
     private javax.swing.JLabel UniversidadeLabel;
     private javax.swing.JLabel jLabel5;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
+    private javax.swing.JComboBox universidadeComboBox;
+    private org.jdesktop.beansbinding.BindingGroup bindingGroup;
     // End of variables declaration//GEN-END:variables
 
     public JTextField getLoginTextField() {
@@ -216,19 +274,167 @@ public class TelaCadastroUsuario extends javax.swing.JFrame {
     }
 
     public JPasswordField getSenhaSenhaPasswordField() {
-        return SenhaPasswordField;
+        return getSenhaPasswordField();
     }
 
     public void setSenhaPasswordField(JPasswordField senhaPasswordField) {
         this.SenhaPasswordField = senhaPasswordField;
     }
 
-    public JComboBox getUniversidadeComboBox() {
-        return UniversidadeComboBox;
+    /**
+     * @return the Cadastrar
+     */
+    public javax.swing.JButton getCadastrar() {
+        return Cadastrar;
     }
 
-    public void setUniversidadeComboBox(JComboBox UniversidadeComboBox) {
-        this.UniversidadeComboBox = UniversidadeComboBox;
+    /**
+     * @param Cadastrar the Cadastrar to set
+     */
+    public void setCadastrar(javax.swing.JButton Cadastrar) {
+        this.Cadastrar = Cadastrar;
     }
+
+    /**
+     * @return the Cancelar
+     */
+    public javax.swing.JButton getCancelar() {
+        return Cancelar;
+    }
+
+    /**
+     * @param Cancelar the Cancelar to set
+     */
+    public void setCancelar(javax.swing.JButton Cancelar) {
+        this.Cancelar = Cancelar;
+    }
+
+    /**
+     * @return the LoginLabel
+     */
+    public javax.swing.JLabel getLoginLabel() {
+        return LoginLabel;
+    }
+
+    /**
+     * @param LoginLabel the LoginLabel to set
+     */
+    public void setLoginLabel(javax.swing.JLabel LoginLabel) {
+        this.LoginLabel = LoginLabel;
+    }
+
+    /**
+     * @return the NomeLabel
+     */
+    public javax.swing.JLabel getNomeLabel() {
+        return NomeLabel;
+    }
+
+    /**
+     * @param NomeLabel the NomeLabel to set
+     */
+    public void setNomeLabel(javax.swing.JLabel NomeLabel) {
+        this.NomeLabel = NomeLabel;
+    }
+
+    /**
+     * @return the SenhaLabel
+     */
+    public javax.swing.JLabel getSenhaLabel() {
+        return SenhaLabel;
+    }
+
+    /**
+     * @param SenhaLabel the SenhaLabel to set
+     */
+    public void setSenhaLabel(javax.swing.JLabel SenhaLabel) {
+        this.SenhaLabel = SenhaLabel;
+    }
+
+    /**
+     * @return the SenhaPasswordField
+     */
+    public javax.swing.JPasswordField getSenhaPasswordField() {
+        return SenhaPasswordField;
+    }
+
+    /**
+     * @return the UniversidadeLabel
+     */
+    public javax.swing.JLabel getUniversidadeLabel() {
+        return UniversidadeLabel;
+    }
+
+    /**
+     * @param UniversidadeLabel the UniversidadeLabel to set
+     */
+    public void setUniversidadeLabel(javax.swing.JLabel UniversidadeLabel) {
+        this.UniversidadeLabel = UniversidadeLabel;
+    }
+
+    /**
+     * @return the jLabel5
+     */
+    public javax.swing.JLabel getjLabel5() {
+        return jLabel5;
+    }
+
+    /**
+     * @param jLabel5 the jLabel5 to set
+     */
+    public void setjLabel5(javax.swing.JLabel jLabel5) {
+        this.jLabel5 = jLabel5;
+    }
+
+    /**
+     * @return the jPanel1
+     */
+    public javax.swing.JPanel getjPanel1() {
+        return jPanel1;
+    }
+
+    /**
+     * @param jPanel1 the jPanel1 to set
+     */
+    public void setjPanel1(javax.swing.JPanel jPanel1) {
+        this.jPanel1 = jPanel1;
+    }
+
+    /**
+     * @return the jPanel2
+     */
+    public javax.swing.JPanel getjPanel2() {
+        return jPanel2;
+    }
+
+    /**
+     * @param jPanel2 the jPanel2 to set
+     */
+    public void setjPanel2(javax.swing.JPanel jPanel2) {
+        this.jPanel2 = jPanel2;
+    }
+
+    /**
+     * @return the universidadeComboBox
+     */
+    public javax.swing.JComboBox getUniversidadeComboBox() {
+        return universidadeComboBox;
+    }
+
+    /**
+     * @param universidadeComboBox the universidadeComboBox to set
+     */
+    public void setUniversidadeComboBox(javax.swing.JComboBox universidadeComboBox) {
+        this.universidadeComboBox = universidadeComboBox;
+    }
+
+    private void setItemUniverdidade(){
+        universidadeComboBox.removeAllItems();
+        universidadeComboBox.addItem("Universidade Católica de Brasília");
+        universidadeComboBox.addItem("Universidade Federal Fluminense");
+        universidadeComboBox.addItem("Universidade Federal de Santa Catarina");
+    }
+
+    
 
 }

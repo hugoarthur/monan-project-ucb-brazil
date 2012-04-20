a25e0133ed35d5ef8961030fdf374d92b3748d2b
gzfreitas@gmail.com <gzfreitas@gmail.com@b4a6f1fb-a3e5-f0e7-49a2-75df67cf75d6>
2012-04-20 17:59:11 +0000

    troca de categoria por celulas

---
+++ TCC_novo/src/br/ucb/beans/Celula.java
@@ -0,0 +1,46 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package br.ucb.beans;
+
+/**
+ *
+ * @author GUICUNHA
+ */
+public class Celula {
+    private Integer idCelula;
+    private String txt_celula;
+
+    /**
+     * @return the idCelula
+     */
+    public Integer getIdCelula() {
+        return idCelula;
+    }
+
+    /**
+     * @param idCelula the idCelula to set
+     */
+    public void setIdCelula(Integer idCelula) {
+        this.idCelula = idCelula;
+    }
+
+    /**
+     * @return the txt_celula
+     */
+    public String getTxt_celula() {
+        return txt_celula;
+    }
+
+    /**
+     * @param txt_celula the txt_celula to set
+     */
+    public void setTxt_celula(String txt_celula) {
+        this.txt_celula = txt_celula;
+    }
+
+   
+   
+}

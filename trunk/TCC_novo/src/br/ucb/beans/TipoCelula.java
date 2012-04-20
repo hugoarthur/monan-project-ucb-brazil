0d146220bc43927ce4aa23db643aba531ad08553
gzfreitas@gmail.com <gzfreitas@gmail.com@b4a6f1fb-a3e5-f0e7-49a2-75df67cf75d6>
2012-04-20 18:02:18 +0000

    troca de categoria por celulas

---
+++ trunk/TCC_novo/src/br/ucb/beans/TipoCelula.java
@@ -0,0 +1,46 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package br.ucb.beans;
+
+/**
+ *
+ * @author GUICUNHA
+ */
+public class TipoCelula {
+    private Integer id_tipo_celula;
+    private String nom_tipo_celula;
+
+    /**
+     * @return the id_tipo_celula
+     */
+    public Integer getId_tipo_celula() {
+        return id_tipo_celula;
+    }
+
+    /**
+     * @param id_tipo_celula the id_tipo_celula to set
+     */
+    public void setId_tipo_celula(Integer id_tipo_celula) {
+        this.id_tipo_celula = id_tipo_celula;
+    }
+
+    /**
+     * @return the nom_tipo_celula
+     */
+    public String getNom_tipo_celula() {
+        return nom_tipo_celula;
+    }
+
+    /**
+     * @param nom_tipo_celula the nom_tipo_celula to set
+     */
+    public void setNom_tipo_celula(String nom_tipo_celula) {
+        this.nom_tipo_celula = nom_tipo_celula;
+    }
+
+    
+
+}

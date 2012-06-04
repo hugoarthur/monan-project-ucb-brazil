c9d0fecc3da0c6330afc9181f4cb12ad88908183
osmarvieirajr@gmail.com <osmarvieirajr@gmail.com@b4a6f1fb-a3e5-f0e7-49a2-75df67cf75d6>
2012-06-04 00:37:54 +0000

    Crie uma classe prar exclusao de usuarios e modifiquei a listagem de usuarios.

--- trunk/TCC_novo/src/br/ucb/gui/ActionListeners/ConfiguraCampos.java
+++ trunk/TCC_novo/src/br/ucb/gui/ActionListeners/ConfiguraCampos.java
@@ -21,22 +21,23 @@ import javax.swing.DefaultListModel;
 public class ConfiguraCampos {
 
     public DefaultListModel listModel() {
+
         DefaultListModel listModel = new DefaultListModel();
-        
+
         if (UsuarioDAO.findAll() != null) {
-            
+
             for (Usuario usuario : UsuarioDAO.findAll()) {
-                
+
                 if (usuario.getTipoUsuario() == 1) {
-                    
+
                     List<Projeto> list = usuario.getProjetos();
-                    
+
                     for (Projeto proj : list) {
-                        
+
                         if (proj.getIdProjeto() == Sessao.getInstance().getProjeto().getIdProjeto()) {
-                            
+
                             listModel.addElement(usuario.getNome());
-                            
+
                         }
                     }
 
@@ -49,12 +50,32 @@ public class ConfiguraCampos {
         return listModel;
     }
 
+    public DefaultListModel listaUser() {
+
+        DefaultListModel listModel = new DefaultListModel();
+
+        if (UsuarioDAO.findAll() != null) {
+
+            for (Usuario usuario : UsuarioDAO.findAll()) {
+                listModel.addElement(usuario.getNome());
+            }
+
+        } else {
+            listModel.addElement("Vazio");
+        }
+        return listModel;
+    }
+
     public DefaultComboBoxModel comboModel() {
+
         DefaultComboBoxModel comboModel = new DefaultComboBoxModel();
+
         if (CelulaDAO.findAll() != null) {
+
             for (Celula celula : CelulaDAO.findAll()) {
                 comboModel.addElement(celula.getTxt_celula());
             }
+
         } else {
             comboModel.addElement("vazio");
         }
@@ -64,6 +85,7 @@ public class ConfiguraCampos {
     public DefaultListModel listModelCel() {
 
         DefaultListModel listModel = new DefaultListModel();
+
         if (CelulaDAO.findAll() != null) {
             for (Celula celula : CelulaDAO.findAll()) {
                 listModel.addElement(celula.getTxt_celula());

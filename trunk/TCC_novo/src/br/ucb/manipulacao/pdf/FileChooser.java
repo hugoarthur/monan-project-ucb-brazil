9a79b0e3a94bacc530aa6b476931a33bfe64baf7
osmarvieirajr@gmail.com <osmarvieirajr@gmail.com@b4a6f1fb-a3e5-f0e7-49a2-75df67cf75d6>
2012-06-09 23:07:38 +0000

    Acompanhamento -> Buscando dados no xml. XML -> Modificado (campo nome do PDF).

--- trunk/TCC_novo/src/br/ucb/manipulacao/pdf/FileChooser.java
+++ trunk/TCC_novo/src/br/ucb/manipulacao/pdf/FileChooser.java
@@ -1,21 +1,33 @@
 package br.ucb.manipulacao.pdf;
 
+import br.ucb.service.Sessao;
 import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
 
 public class FileChooser {
 
     public String caminho() {
+        
         String caminhoArquivo = "";
+        
         JFileChooser arquivo = new JFileChooser();
-         arquivo.setFileFilter(new ExtensionFileFilter("Arquivos PDF","pdf"));
+        arquivo.setFileFilter(new ExtensionFileFilter("Arquivos PDF", "pdf"));
+        
         int retorno = arquivo.showOpenDialog(null);
+        
         if (retorno == JFileChooser.APPROVE_OPTION) {
+            
             caminhoArquivo = arquivo.getSelectedFile().getAbsolutePath();
+            Sessao.getInstance().setNomeArquivoAtual(arquivo.getSelectedFile().getName());
+            
         } else {
+            
             JOptionPane.showMessageDialog(null, "Voce nao selecionou nenhum Arquivo PDF.");
             caminhoArquivo = "";
+            
         }
+        
         return caminhoArquivo;
+        
     }
 }

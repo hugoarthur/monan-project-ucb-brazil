2a533d1148349c4be364574543ef8fc17dd3601d
osmarvieirajr@gmail.com <osmarvieirajr@gmail.com@b4a6f1fb-a3e5-f0e7-49a2-75df67cf75d6>
2012-06-14 20:50:10 +0000

    Registrar Relatorio de Acompanhamento Correçao do erro do exportar xml.

---
+++ trunk/TCC_novo/src/br/ucb/dao/AcompanhamentoDAO.java
@@ -0,0 +1,42 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.ucb.dao;
+
+import br.ucb.beans.Acompanhamento;
+import javax.persistence.Query;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author uc07053438
+ */
+public class AcompanhamentoDAO {
+
+    public void insereAcompanhamento(Acompanhamento acompanhamento) {
+        EntityManager em = DataBase.getInstance().getEntityManager();
+        if (!em.getTransaction().isActive()) {
+            em.getTransaction().begin();
+        }
+        em.persist(acompanhamento);
+        em.getTransaction().commit();
+        em.close();
+    }
+
+    public static List<Acompanhamento> findAll() {
+        EntityManager em = DataBase.getInstance().getEntityManager();
+        Query query = em.createQuery("SELECT c FROM Acompanhamento c");
+        List<Acompanhamento> celulas = null;
+        try {
+            celulas = (List<Acompanhamento>) query.getResultList();
+            return celulas;
+        } catch (javax.persistence.NoResultException e) {
+            JOptionPane.showMessageDialog(null, "É preciso cadastrar célula", "Warning", JOptionPane.WARNING_MESSAGE);
+        }
+        return celulas;
+
+    }
+}
